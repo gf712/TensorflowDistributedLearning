850cca04a3cae5e9e1f6e4965ae436e142fa2be4
Gil <g.f.hoben@gmail.com>
2018-10-10 17:27:02 +0100

    added model params to Model class

--- model.py
+++ model.py
@@ -3,8 +3,8 @@ from utils import get_available_gpus, metric_comparisson
 from core.resnet import resnet_model
 from core.losses import lovasz_loss
 from core.metric import mIOU, mean_accuracy
-from preprocessing.preprocessing import _prepare_directory, read_and_preprocess, single_transformation, \
-    create_symlinks
+from preprocessing.preprocessing import _prepare_directory, read_and_preprocess, single_transformation_from_jpeg, \
+    single_transformation_from_matrix, create_symlinks
 from sklearn.model_selection import StratifiedKFold
 import functools
 
@@ -34,7 +34,8 @@ class Model:
                  n_gpus=2,
                  n_fold=5,
                  seed=42,
-                 save_best=5):
+                 save_best=5,
+                 **kwargs):
         """
         High level class to perform multi GPU training with tf.contrib.distribute.MirroredStrategy
         and tf.Estimator.
@@ -42,7 +43,7 @@ class Model:
         All the data is processed using tf.data.Dataset and tf.image.
         The preprocessing currently runs on the CPU (optimal?)
         Additionally built ResNet with NCHW format support for (potentially) faster GPU and MKL optimised CPU operations
-        Currently NCHW format support is experimental and the speed up is minor (about 5-10%)
+        Currently NCHW format support is experimental and the speed up is minor (about 10%)
 
 
         :param model_dir:
@@ -58,6 +59,51 @@ class Model:
             self.data_format = data_format
         else:
             raise ValueError(f"Unknown data format {data_format}. Has to be either NCHW or NHWC")
+
+        if "weight_decay" in kwargs:
+            self.weight_decay = kwargs["weight_decay"]
+        else:
+            self.weight_decay = WEIGHT_DECAY
+
+        if "batch_norm_decay" in kwargs:
+            self.batch_norm_decay = kwargs["weight_decay"]
+        else:
+            self.batch_norm_decay = BATCH_NORM_DECAY
+
+        if "batch_norm_epsilon" in kwargs:
+            self.batch_norm_epsilon = kwargs["batch_norm_epsilon"]
+        else:
+            self.batch_norm_epsilon = BATCH_NORM_EPSILON
+
+        if "batch_norm_scale" in kwargs:
+            self.batch_norm_scale = kwargs["batch_norm_scale"]
+        else:
+            self.batch_norm_scale = BATCH_NORM_SCALE
+
+        if "output_stride" in kwargs:
+            self.output_stride = kwargs["output_stride"]
+        else:
+            self.output_stride = OUTPUT_STRIDE
+
+        if "base_depth" in kwargs:
+            self.base_depth = kwargs["base_depth"]
+        else:
+            self.base_depth = BASE_DEPTH
+
+        if "input_shape" in kwargs:
+            self.input_shape = kwargs["input_shape"]
+        else:
+            self.input_shape = INPUT_SHAPE
+
+        if "n_blocks" in kwargs:
+            self.n_blocks = kwargs["n_blocks"]
+        else:
+            self.n_blocks = (3, 4, 6)
+
+        if "block_type" in kwargs:
+            self.block_type = kwargs["block_type"]
+        else:
+            self.block_type = "bottleneck"
 
         # Pathing stuff
         self.model_name = model_dir.split('/')[-1]
@@ -120,7 +166,8 @@ class Model:
                 model_dir=f"{self.model_dir}/fold{i}",
                 config=self.config,
                 params={'fold': i,
-                        'threshold': 0.5  # probability threshold for positive classification
+                        'threshold': 0.5,  # probability threshold for positive classification
+                        'block_type': "basic_block"
                         }
             )
 
@@ -164,8 +211,8 @@ class Model:
                                              augment=False,
                                              shuffle=False),
                 steps=len(test_index) // (batch_size * 2),
-                # throttle_secs=120,
-                # start_delay_secs=120,
+                throttle_secs=300,
+                start_delay_secs=0,
                 exporters=exporter
             )
 
@@ -179,7 +226,7 @@ class Model:
 
             i += 1
 
-    # TODO: need to finish writing this method
+    # TODO: finish writing this method
     def predict(self, test_dir, batch_size, tti=False):
         """
         Runs evaluation with or without test time augmentation.
@@ -205,7 +252,6 @@ class Model:
                     params={'fold': i,
                             'transformation': transformation}
                 )
-
                 result_iterator = model.predict(input_fn=input_fn)
 
     def _make_test_input(self, batch_size, test_directory, tti):
@@ -224,9 +270,11 @@ class Model:
 
             dataset = tf.data.Dataset.from_tensor_slices(filenames)
 
-            dataset = dataset.map(functools.partial(single_transformation, {'transformation': tti}))
+            dataset = dataset.map(map_func=functools.partial(single_transformation_from_jpeg,
+                                                             transformation=tti))
+            tf.logging.info(dataset)
 
-            dataset = dataset.repeat()
+            # dataset = dataset.repeat()
             dataset = dataset.batch(batch_size)
             dataset = dataset.prefetch(self.n_gpus * 2)  # prefetch twice as many batches as there are GPUs
 
@@ -264,7 +312,9 @@ class Model:
             #                                   num_parallel_calls=os.cpu_count()
             #                                   ))
 
-            dataset = dataset.map(functools.partial(read_and_preprocess, augment=augment)).batch(batch_size)
+            dataset = dataset.map(functools.partial(read_and_preprocess,
+                                                    augment=augment,
+                                                    crop_probability=0)).batch(batch_size)
 
             # prefetch twice as many batches as there are GPUs -> doesn't always help and might need adjustment
             dataset = dataset.prefetch(self.n_gpus * 2)
@@ -277,7 +327,8 @@ class Model:
 
         def _model_fn(features, labels, mode, params):
 
-            fold = params["fold"]
+            if "fold" in params:
+                fold = params["fold"]
             # treshold is used to determine when a prediction is negative/positive (0/1)
             if "threshold" in params:
                 threshold = params["threshold"]
@@ -290,30 +341,32 @@ class Model:
             else:
                 is_training = False
 
-            with tf.variable_scope(self.model_name):
+            if self.data_format == "NCHW":
+                input = tf.transpose(features["images"], [0, 3, 1, 2], name="image_input")
+                if mode != tf.estimator.ModeKeys.PREDICT:
+                    labels = tf.transpose(labels, [0, 3, 1, 2], name="label_input")
+            else:
+                input = tf.identity(features["images"], name="image_input")
+                if mode != tf.estimator.ModeKeys.PREDICT:
+                    labels = tf.identity(labels, name="label_input")
 
-                if self.data_format == "NCHW":
-                    input = tf.transpose(features["images"], [0, 3, 1, 2], name="image_input")
-                    if not tf.estimator.ModeKeys.EVAL:
-                        labels = tf.transpose(labels, [0, 3, 1, 2], name="label_input")
-                else:
-                    input = tf.identity(features["images"], name="image_input")
-                    if not tf.estimator.ModeKeys.EVAL:
-                        labels = tf.identity(labels, name="label_input")
+            with tf.variable_scope(self.model_name):
 
                 # Model definition
                 preactivation_output = resnet_model(
                     input=input,
                     model_name=self.model_name,
-                    weight_decay=WEIGHT_DECAY,
-                    batch_norm_decay=BATCH_NORM_DECAY,
-                    batch_norm_epsilon=BATCH_NORM_EPSILON,
-                    batch_norm_scale=BATCH_NORM_SCALE,
+                    weight_decay=self.weight_decay,
+                    batch_norm_decay=self.batch_norm_decay,
+                    batch_norm_epsilon=self.batch_norm_epsilon,
+                    batch_norm_scale=self.batch_norm_scale,
                     data_format=self.data_format,
                     is_training=is_training,
-                    output_stride=OUTPUT_STRIDE,
-                    base_depth=BASE_DEPTH,
-                    input_shape=INPUT_SHAPE
+                    output_stride=self.output_stride,
+                    base_depth=self.base_depth,
+                    input_shape=self.input_shape,
+                    n_blocks=self.n_blocks,
+                    block_type=self.block_type
                 )
                 output = tf.nn.sigmoid(preactivation_output, name='sigmoid_output')
                 predicted = tf.to_float(tf.greater(output, threshold))
@@ -330,7 +383,8 @@ class Model:
 
                 # reverse test time augmentation -> this works for all flipping of images along a given axis
                 # transpose, horizontal and vertical flip (and no augmentation)
-                output = single_transformation(predicted, params["transformation"])
+                output = single_transformation_from_matrix(output, params["transformation"])["images"]
+                predicted = single_transformation_from_matrix(predicted, params["transformation"])["images"]
 
             elif mode in (tf.estimator.ModeKeys.EVAL, tf.estimator.ModeKeys.TRAIN):
 
@@ -355,8 +409,8 @@ class Model:
                     # write out images (convert to NHWC format, only format supported by tf.image)
                     if self.data_format == "NCHW":
                         s_op.extend([tf.summary.image(f"{mode}_image",
-                                                      tf.transpose(input[..., :1],
-                                                                   perm=[0, 2, 3, 1]),
+                                                      tf.transpose(input,
+                                                                   perm=[0, 2, 3, 1])[..., :1],
                                                       max_outputs=1),
                                      tf.summary.image(f"{mode}_label",
                                                       tf.transpose(labels,
@@ -386,6 +440,10 @@ class Model:
 
                 if mode == tf.estimator.ModeKeys.TRAIN:
 
+                    # get number of parameters in model
+                    self.n_params = sum(functools.reduce(lambda x, y: x * y, x.get_shape().as_list()) for x in
+                                        tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.model_name))
+
                     # add metric ops to add to train hooks -> this allows eval and train set metrics to be in
                     # the same plot in tensorboard
                     with tf.variable_scope("metrics"):
@@ -401,7 +459,7 @@ class Model:
                                                     name='learning_rate')
 
                     # define optimiser
-                    optimizer = tf.train.AdamOptimizer(lr)
+                    optimizer = tf.contrib.optimizer_v2.AdamOptimizer(lr)
 
                     # add batch norm moving average and variance to update ops
                     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
@@ -445,3 +503,10 @@ class Model:
 
         # return function with signature f(features, labels, mode, params): -> tf.estimator.EstimatorSpec
         return _model_fn
+
+    @property
+    def params(self):
+        try:
+            return self.n_params
+        except:
+            raise ValueError("No model has been defined at this point! Call train method first.")
